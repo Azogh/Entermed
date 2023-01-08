f17c16e3bed2b05f3a021e69c0a328258e33f885
André Gustavo Silva Ozga <106564740+Azogh@users.noreply.github.com>
2023-01-08 20:21:21 -0300

    Update manual projeto

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$('.slider').slick({
+
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: true,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    atoplayspeed: 2000
+});
